581c4371e33775250f3574205582ce29c3ee3aa1
Lucas Käldström <luxas@users.noreply.github.com>
2019-08-07 20:09:52 +0300

    Dockerize the generator, and add Turku (#6)
    * Dockerize the generation process
    
    * Add turku
    
    * Fix a small bug
    
    * Add Stockholm and Copenhagen

--- slide-background/generator/capture.js
+++ slide-background/generator/capture.js
@@ -8,15 +8,15 @@ function timeout(ms) {
 }
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   page.setViewport({
     width: 3840,
     height: 2160
   });
-  await page.goto(`${url.pathToFileURL('background/index.html')}`);
+  await page.goto(`http://localhost`);
   await timeout(1000);
-  await page.screenshot({ path: `${process.env.LOCATION}.png` });
+  await page.screenshot({ path: `screenshot.png` });
 
   await browser.close();
 })();
